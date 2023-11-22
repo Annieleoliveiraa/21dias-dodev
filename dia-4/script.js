e701d6b71917b907c272244d0166eead575afb25
Anniely Oliveira <annieleoliveirareiis@gmail.com>
2023-11-22 09:00:19 -0300

    adiconando as alterações

---
+++ dia-4/script.js
@@ -0,0 +1,10 @@
+
+let idade = Number(prompt("Digite sua idade"))
+// == igual | != diferente | < menor que e > maior que | <= >= maior e menor, ou igual
+// === igual ao valor e ao tipo | !== é diferente o valor e o tipo 
+
+if(idade <= 10) {
+  console.log('você tem 10 anos ou menos')
+} else {
+  console.log('você é maior de 10 anos')
+}
